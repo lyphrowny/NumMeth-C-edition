b39b59a231134b086e76ef7634c9ad05522b219a
lyphrowny <strukov.as@edu.spbstu.ru>
2021-04-29 14:08:11 +0300

    stub funcs for math mat operations

---
+++ lab3/lab3.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lab3.h"
+
+float dotProduct(const size_t* cols, const float** vec1, const float** vec2) {
+
+}
+
+float** sc_matMul(const float* scal, const size_t* rows, const size_t* cols, const float** mat) {
+
+}
+
+// [a, b] * [b, c] -> [a, c]
+float** mat_matMul(const size_t* rows, const size_t* _cols, const size_t* cols, const float** mat1, const float** mat2) {
+
+}
+
+float** mat_matSub(const size_t* rows1, const size_t* cols1, const float** mat1, const size_t* rows2, const size_t* cols2, const float** mat2) {
+
+}
---
+++ lab3/main.c
@@ -0,0 +1,6 @@
+#include "lab3.h"
+
+int main() {
+
+    return 0;
+}
